d4a21ef50f6733114297f4847a91836e9fd3f3e6
Hannu Eloranta <hannu.eloranta@gmail.com>
2025-05-31 09:18:41 +0300

    add equations

--- src/app/moon-calculator/moon-calculator.ts
+++ src/app/moon-calculator/moon-calculator.ts
@@ -1,4 +1,440 @@
-import { Component } from '@angular/core';
+import {Component, OnChanges, SimpleChanges, Input, ViewChild} from '@angular/core'
+import { BaseChartDirective } from 'ng2-charts'
+
+function div(x: number, y: number) {
+  return ~~(x / y) // integer division
+}
+
+function rev(x: number) {
+  return x - Math.floor(x / 360) * 360
+}
+
+function toDegrees(x: number) {
+  return 180 * x / Math.PI
+}
+
+function toTime(degrees: number) {
+ let hours = Math.floor(degrees / 15)
+ let minutes = (degrees / 15 - hours) * 60
+ return hours + ":" + Math.floor(minutes)
+}
+
+function sin(x: number) {
+  return Math.sin(Math.PI * x / 180)
+}
+
+function cos(x: number) {
+  return Math.cos(Math.PI * x / 180)
+}
+
+function tan(x: number) {
+  return Math.tan(Math.PI * x / 180)
+}
+
+function asin(x: number) {
+  return toDegrees(Math.asin(x))
+}
+
+function atan(x: number) {
+  return toDegrees(Math.atan(x))
+}
+
+function atan2(x: number, y: number) {
+  return toDegrees(Math.atan2(x, y))
+}
+
+class Earth {
+  obliquity(dayNumber: number) {
+    return 23.4393 - 3.563e-7 * dayNumber
+  }
+
+  gclat(latitude: number) {
+    return latitude - 0.1924 * sin(2 * latitude)
+  }
+
+  rho(latitude: number) {
+    return 0.99833 + 0.00167 * cos(2 * latitude)
+  }
+}
+
+class Sun {
+  longitudeOfPerihelion(dayNumber: number) {
+    return 282.9404 + 4.70935e-5 * dayNumber
+  }
+
+  eccentricity(dayNumber: number) {
+    return 0.016709 - 1.151e-9 * dayNumber
+  }
+
+  meanAnomaly(dayNumber: number) {
+    return rev(356.0470 + 0.9856002585 * dayNumber)
+  }
+
+  meanLongitude(dayNumber: number) {
+    return rev(this.longitudeOfPerihelion(dayNumber) + this.meanAnomaly(dayNumber))
+  }
+
+  eccentricAnomaly(dayNumber: number) {
+    let M = this.meanAnomaly(dayNumber)
+    let e = this.eccentricity(dayNumber)
+    let E0 = M + (180 / Math.PI) * e * sin(M) * (1 + e * cos(M))
+    return E0 // Schlyter
+    // return E0 - (E0 - 180 * e * sin(E0) / Math.PI - M) / (1 - e * cos(E0))  // Taylor
+  }
+  
+  x(dayNumber: number) {
+    return cos(this.eccentricAnomaly(dayNumber)) - this.eccentricity(dayNumber)
+  }
+
+  y(dayNumber: number) {
+    let e = this.eccentricity(dayNumber)
+    return sin(this.eccentricAnomaly(dayNumber)) * Math.sqrt(1 - e * e)
+  }
+
+  distance(dayNumber: number) {
+    let x = this.x(dayNumber)
+    let y = this.y(dayNumber)
+    return Math.sqrt(x * x + y * y)
+  }
+
+  trueAnomaly(dayNumber: number) {
+    let x = this.x(dayNumber)
+    let y = this.y(dayNumber)
+    return atan2(y, x)
+  }
+
+  longitude(dayNumber: number) {
+    return rev(this.trueAnomaly(dayNumber) + this.longitudeOfPerihelion(dayNumber))
+  }
+
+  xEcliptic(dayNumber: number) {
+    return this.distance(dayNumber) * cos(this.longitude(dayNumber))
+  }
+
+  yEcliptic(dayNumber: number) {
+    return this.distance(dayNumber) * sin(this.longitude(dayNumber))
+  }
+
+  xEquatorial(dayNumber: number) {
+    return this.xEcliptic(dayNumber)
+  }
+
+  yEquatorial(dayNumber: number, earth: Earth) {
+    return this.yEcliptic(dayNumber) * cos(earth.obliquity(dayNumber))
+  }
+
+  zEquatorial(dayNumber: number, earth: Earth) {
+    return this.yEcliptic(dayNumber) * sin(earth.obliquity(dayNumber))
+  }
+
+  rightAscension(dayNumber: number, earth: Earth) {
+    return rev(atan2(this.yEquatorial(dayNumber, earth), this.xEquatorial(dayNumber)))
+  }
+  
+  declination(dayNumber: number, earth: Earth) {
+    let x = this.xEquatorial(dayNumber)
+    let y = this.yEquatorial(dayNumber, earth)
+    return atan2(this.zEquatorial(dayNumber, earth), Math.sqrt((x * x + y * y)))
+  }
+
+  GMST0(dayNumber: number) {
+    return this.meanLongitude(dayNumber) / 15 + 12
+  }
+  
+  localSiderealTime(dayNumber: number, longitude: number) {
+    let UT = (dayNumber % 1) * 24
+    return this.GMST0(dayNumber) + UT + longitude / 15
+  }
+  
+  hourAngle(dayNumber: number, longitude: number, earth: Earth) {
+    return rev(this.localSiderealTime(dayNumber, longitude) * 15 - this.rightAscension(dayNumber, earth))
+  }
+  
+  x2(dayNumber: number, longitude: number, earth: Earth) {
+    return cos(this.hourAngle(dayNumber, longitude, earth)) * cos(this.declination(dayNumber, earth))
+  }
+
+  y2(dayNumber: number, longitude: number, earth: Earth) {
+    return sin(this.hourAngle(dayNumber, longitude, earth)) * cos(this.declination(dayNumber, earth))
+  }
+
+  z2(dayNumber: number, longitude: number, earth: Earth) {
+    return sin(this.declination(dayNumber, earth))
+  }
+
+  xHorizontal(dayNumber: number, longitude: number, latitude: number, earth: Earth) {
+    return this.x2(dayNumber, longitude, earth) * sin(latitude) - this.z2(dayNumber, longitude, earth) * cos(latitude)
+  }
+
+  yHorizontal(dayNumber: number, longitude: number, latitude: number, earth: Earth) {
+    return this.y2(dayNumber, longitude, earth)
+  }
+
+  zHorizontal(dayNumber: number, longitude: number, latitude: number, earth: Earth) {
+    return this.x2(dayNumber, longitude, earth) * cos(latitude) + this.z2(dayNumber, longitude, earth) * sin(latitude)
+  }
+
+  azimuth(dayNumber: number, longitude: number, latitude: number, earth: Earth) {
+    return rev(atan2(this.yHorizontal(dayNumber, longitude, latitude, earth), this.xHorizontal(dayNumber, longitude, latitude, earth)) + 180.0)
+  }
+
+  elevation(dayNumber: number, longitude: number, latitude: number, earth: Earth) {
+    return asin(this.zHorizontal(dayNumber, longitude, latitude, earth))
+  }
+}
+
+class Moon {
+  longitudeOfAscendingNode(dayNumber: number) {
+    return 125.1228 - 0.0529538083 * dayNumber
+  }
+
+  inclination = 5.1454
+  
+  argumentOfPerigee(dayNumber: number) {
+    return 318.0634 + 0.1643573223 * dayNumber
+  }
+
+  meanDistance = 60.2666
+  eccentricity = 0.054900
+
+  meanAnomaly(dayNumber: number) {
+    return rev(115.3654 + 13.0649929509 * dayNumber)
+  }
+
+  E0(dayNumber: number) {
+    let M = this.meanAnomaly(dayNumber)
+    let e = this.eccentricity
+    return M + (180 / Math.PI) * e * sin(M) * (1 + e * cos(M))
+  }
+  
+  E1(dayNumber: number, E0: number) {
+    let M = this.meanAnomaly(dayNumber)
+    let e = this.eccentricity
+    return E0 - (E0 - (180.0 / Math.PI) * e * sin(E0) - M) / (1 - e * cos(E0))
+  }
+
+  E(dayNumber: number) {
+    let E = this.E0(dayNumber)
+    E = this.E1(dayNumber, E)
+    E = this.E1(dayNumber, E)
+    return E
+  }
+
+  x(dayNumber: number) {
+    return this.meanDistance * (cos(this.E(dayNumber)) - this.eccentricity)
+  }
+  
+  y(dayNumber: number) {
+    let e = this.eccentricity
+    return this.meanDistance * Math.sqrt(1 - e * e) * sin(this.E(dayNumber))
+  }
+
+  r(dayNumber: number) {
+    let x = this.x(dayNumber)
+    let y = this.y(dayNumber)
+    return Math.sqrt(x * x + y * y)
+  }
+  
+  v(dayNumber: number) {
+    let x = this.x(dayNumber)
+    let y = this.y(dayNumber)
+    return rev(atan2(y, x))
+  }
+
+  xeclip(dayNumber: number) {
+    let N = this.longitudeOfAscendingNode(dayNumber)
+    let v = this.v(dayNumber)
+    let w = this.argumentOfPerigee(dayNumber)
+    return this.r(dayNumber) * (cos(N) * cos(v + w) - sin(N) * sin(v + w) * cos(this.inclination))
+  }
+
+  yeclip(dayNumber: number) {
+    let N = this.longitudeOfAscendingNode(dayNumber)
+    let v = this.v(dayNumber)
+    let w = this.argumentOfPerigee(dayNumber)
+    return this.r(dayNumber) * (sin(N) * cos(v + w) + cos(N) * sin(v + w) * cos(this.inclination))
+  }
+  
+  zeclip(dayNumber: number) {
+    let v = this.v(dayNumber)
+    let w = this.argumentOfPerigee(dayNumber)
+    return this.r(dayNumber) * sin(v + w) * sin(this.inclination)
+  }
+
+  longitudeEcl(dayNumber: number) {
+    return rev(atan2(this.yeclip(dayNumber), this.xeclip(dayNumber)))
+  }
+
+  latitudeEcl(dayNumber: number) {
+    let x = this.xeclip(dayNumber)
+    let y = this.yeclip(dayNumber)
+    return atan2(this.zeclip(dayNumber), Math.sqrt(x * x + y * y))
+  }
+
+  meanLongitude(dayNumber: number) {
+    return this.longitudeOfAscendingNode(dayNumber) + this.argumentOfPerigee(dayNumber) + this.meanAnomaly(dayNumber)
+  }
+
+  meanElongation(dayNumber: number, sun: Sun) {
+    return this.meanLongitude(dayNumber) - sun.meanLongitude(dayNumber)
+  }
+
+  argumentOfLatitude(dayNumber: number) {
+    return this.meanLongitude(dayNumber) - this.longitudeOfAscendingNode(dayNumber)
+  }
+  
+  dLongitude(dayNumber: number, sun: Sun) {
+    let Mm = this.meanAnomaly(dayNumber)
+    let D = this.meanElongation(dayNumber, sun)
+    let Ms = sun.meanAnomaly(dayNumber)
+    let F = this.argumentOfLatitude(dayNumber)
+    return -1.274 * sin(Mm - 2*D)
+      +0.658 * sin(2*D)
+      -0.186 * sin(Ms)
+      -0.059 * sin(2*Mm - 2*D)
+      -0.057 * sin(Mm - 2*D + Ms)
+      +0.053 * sin(Mm + 2*D)
+      +0.046 * sin(2*D - Ms)
+      +0.041 * sin(Mm - Ms)
+      -0.035 * sin(D)
+      -0.031 * sin(Mm + Ms)
+      -0.015 * sin(2*F - 2*D)
+      +0.011 * sin(Mm - 4*D)
+  }
+
+  dLatitude(dayNumber: number, sun: Sun) {
+    let Mm = this.meanAnomaly(dayNumber)
+    let D = this.meanElongation(dayNumber, sun)
+    let F = this.argumentOfLatitude(dayNumber)
+    return -0.173 * sin(F - 2*D)
+      -0.055 * sin(Mm - F - 2*D)
+      -0.046 * sin(Mm + F - 2*D)
+      +0.033 * sin(F + 2*D)
+      +0.017 * sin(2*Mm + F)
+  }
+  
+  dDistance(dayNumber: number, sun: Sun) {
+    let Mm = this.meanAnomaly(dayNumber)
+    let D = this.meanElongation(dayNumber, sun)
+    return -0.58 * cos(Mm - 2*D)   // TODO: difference here between Schlyter and Taylor
+      -0.46 * cos(2*D)
+  }
+
+  longitude(dayNumber: number, sun: Sun) {
+    return this.longitudeEcl(dayNumber) + this.dLongitude(dayNumber, sun)
+  }
+
+  latitude(dayNumber: number, sun: Sun) {
+    return this.latitudeEcl(dayNumber) + this.dLatitude(dayNumber, sun)
+  }
+
+  distance(dayNumber: number, sun: Sun) {
+    return this.r(dayNumber) + this.dDistance(dayNumber, sun)
+  }
+
+  X(dayNumber: number, sun: Sun) {
+    return cos(this.latitude(dayNumber, sun)) * cos(this.longitude(dayNumber, sun))
+  }
+
+  Y(dayNumber: number, sun: Sun) {
+    return cos(this.latitude(dayNumber, sun)) * sin(this.longitude(dayNumber, sun))
+  }
+
+  Z(dayNumber: number, sun: Sun) {
+    return sin(this.latitude(dayNumber, sun))
+  }
+
+  xEquat(dayNumber: number, sun: Sun) {
+    return this.X(dayNumber, sun)
+  }
+
+  yEquat(dayNumber: number, sun: Sun, earth: Earth) {
+    let obliquity = earth.obliquity(dayNumber)
+    return this.Y(dayNumber, sun) * cos(obliquity) - this.Z(dayNumber, sun) * sin(obliquity)
+  }
+
+  zEquat(dayNumber: number, sun: Sun, earth: Earth) {
+    let obliquity = earth.obliquity(dayNumber)
+    return this.Y(dayNumber, sun) * sin(obliquity) + this.Z(dayNumber, sun) * cos(obliquity)
+  }
+
+  rightAscension(dayNumber: number, sun: Sun, earth: Earth) {
+   return rev(atan2(this.yEquat(dayNumber, sun, earth), this.xEquat(dayNumber, sun)))
+  }
+
+  declination(dayNumber: number, sun: Sun, earth: Earth) {
+    let x = this.xEquat(dayNumber, sun)
+    let y = this.yEquat(dayNumber, sun, earth)
+    return atan2(this.zEquat(dayNumber, sun, earth), Math.sqrt(x * x + y * y))
+  }
+
+  mpar(dayNumber: number, sun: Sun) {
+    return asin(1/this.distance(dayNumber, sun))
+  }
+
+  HA(dayNumber: number, longitude: number, sun: Sun, earth: Earth) {
+    let LST = sun.localSiderealTime(dayNumber, longitude) * 15  // this belongs to sun ?
+    let RA = this.rightAscension(dayNumber, sun, earth)
+    return rev(LST - RA)
+  }
+
+  g(dayNumber: number, longitude: number, latitude: number, sun: Sun, earth: Earth) {
+    let gclat = earth.gclat(latitude)
+    let HA = this.HA(dayNumber, longitude, sun, earth)
+    return atan(tan(gclat) / cos(HA))
+  }
+  
+  topRA(dayNumber: number, longitude: number, latitude: number, sun: Sun, earth: Earth) {
+    let RA = this.rightAscension(dayNumber, sun, earth)
+    let mpar = this.mpar(dayNumber, sun)
+    let rho = earth.rho(latitude)
+    let gclat = earth.gclat(latitude)
+    let HA = this.HA(dayNumber, longitude, sun, earth)
+    let decl = this.declination(dayNumber, sun, earth)
+    return RA - mpar * rho * cos(gclat) * sin(HA) / cos(decl)
+  }
+
+  topDecl(dayNumber: number, longitude: number, latitude: number, sun: Sun, earth: Earth) {
+    let decl = this.declination(dayNumber, sun, earth)
+    let mpar = this.mpar(dayNumber, sun)
+    let rho = earth.rho(latitude)
+    let gclat = earth.gclat(latitude)
+    let g = this.g(dayNumber, longitude, latitude, sun, earth)
+    return decl - mpar * rho * sin(gclat) * sin(g - decl) / sin(g)
+  }
+ 
+  HA2(dayNumber: number, longitude: number, latitude: number, sun: Sun, earth: Earth) {
+   let lst = sun.localSiderealTime(dayNumber, longitude) * 15
+   let ra = this.topRA(dayNumber, longitude, latitude, sun, earth)
+   let ha = lst - ra
+   if (ha > 180) ha = ha - 360
+   if (ha < 180) ha = ha + 360
+   return ha
+  }
+
+  azimuth(dayNumber: number, longitude: number, latitude: number, sun: Sun, earth: Earth) {
+    let temp1 = sin(this.topDecl(dayNumber, longitude, latitude, sun, earth))
+    let temp2 = cos(this.topDecl(dayNumber, longitude, latitude, sun, earth))
+    let temp3 = sin(latitude) * temp1 + cos(latitude) * temp2 * cos(this.HA2(dayNumber, longitude, latitude, sun, earth))
+    let temp4 = Math.sqrt(1 - temp3 * temp3)
+    let temp5 = -sin(this.HA2(dayNumber, longitude, latitude, sun, earth)) * temp2 / temp4
+    let temp6 = (temp1 - temp3 * sin(latitude)) / (temp4*cos(latitude))
+    let temp7 = 0
+    if (temp6 < 0) temp7 = (1 - temp6)/temp5
+    if (temp6 > 0) temp7 = temp5 / (1 + temp6)
+    let azimuth = 2 * atan(temp7)
+    if (azimuth < 0) azimuth = azimuth + 360
+    return azimuth
+ }
+
+  elevation(dayNumber: number, longitude: number, latitude: number, sun: Sun, earth: Earth) {
+    let temp1 = sin(this.topDecl(dayNumber, longitude, latitude, sun, earth))
+    let temp2 = cos(this.topDecl(dayNumber, longitude, latitude, sun, earth))
+    let temp3 = sin(latitude) * temp1 + cos(latitude) * temp2 * cos(this.HA2(dayNumber, longitude, latitude, sun, earth))
+    let temp4 = Math.sqrt(1 - temp3 * temp3)
+    return atan(temp3 / temp4)
+    }
+}
 
 @Component({
   selector: 'app-moon-calculator',
@@ -6,6 +442,7 @@ import { Component } from '@angular/core';
   templateUrl: './moon-calculator.html',
   styleUrl: './moon-calculator.css'
 })
+
 export class MoonCalculator {
 
 }
